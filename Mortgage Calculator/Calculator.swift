0452d1444392c2639b21163d3b5b8b0ba3095fc2
boshoff7 <boshoff7@gmail.com>
2022-06-07 20:26:59 +0200

    Initial Commit

---
+++ Mortgage Calculator/Calculator.swift
@@ -0,0 +1,40 @@
+//
+//  Calculator.swift
+//  Mortgage Calculator
+//
+//  Created by Chris Boshoff on 2022/06/07.
+//
+
+import Foundation
+
+class Calculator {
+    
+    func calculate(price: Double, deposit: Double, interest: Double, loanTerm: Double) -> Double {
+        
+        let totalPrice = price - deposit
+        print("Total Price: \(totalPrice)")
+        let interests = (interest / 100) / 12
+        let result = totalPrice * (interests * (interestOne(interest: interest, loanTerm: loanTerm))/interestTwo(interest: interest, loanTerm: loanTerm))
+        
+        return result
+    }
+    
+    func interestOne(interest: Double, loanTerm: Double) -> Double {
+        
+        let totalInterest = 1 + ((interest / 100) / 12)
+        let totalLoanTerm = loanTerm * 12
+        let final = pow(totalInterest, totalLoanTerm)
+        
+        return final
+    }
+    
+    func interestTwo(interest: Double, loanTerm: Double) -> Double {
+        
+        let totalInterest = 1 + ((interest / 100) / 12)
+        let totalLoanTerm = loanTerm * 12
+        let final = pow(totalInterest, totalLoanTerm)
+        let answer = final - 1
+        
+        return answer
+    }
+}
--- Mortgage Calculator/ViewController.swift
+++ Mortgage Calculator/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var purchasePriceTextField: UITextField!
+    @IBOutlet weak var depositTextField: UITextField!
+    @IBOutlet weak var interestTextField: UITextField!
+    @IBOutlet weak var loanTermLabel: UILabel!
+    @IBOutlet weak var loanTermTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var yearsSlider: UISlider!
+    
+    let calculator = Calculator()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var price: Double? {
+        return Double(purchasePriceTextField.text!)
+    }
+    var deposit: Double? {
+        return Double(depositTextField.text!)
+    }
+    var interest: Double? {
+        return Double(interestTextField.text!)
+    }
+    var loanTerm: Double? {
+        return Double(loanTermTextField.text!)
+    }
+    
 
-
+    @IBAction func calculateTapped(_ sender: Any) {
+        
+        if let price = self.price,
+           let deposit = self.deposit,
+           let interest = self.interest,
+           let loanTerm = self.loanTerm
+        {
+            let result = calculator.calculate(price: price, deposit: deposit, interest: interest, loanTerm: loanTerm)
+            print(result)
+            //resultLabel.text = String(result)
+            resultLabel.text = String(format: "%.2f", result)
+            
+        }
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
+        let years = String(format: "%.0f", sender.value)
+        loanTermTextField.text = "\(years)"
+        
+    }
+    
 }
 
